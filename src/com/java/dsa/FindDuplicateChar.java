de231732615057970c111bb2ffdd0a2b921db6cc
ganesh-2709 <ganeshrathod28138@gmail.com>
2026-03-07 10:36:32 +0530

    print unique and duplicate seperatly

--- src/com/java/dsa/FindDuplicateChar.java
+++ src/com/java/dsa/FindDuplicateChar.java
@@ -1,7 +1,6 @@
 package com.java.dsa;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class FindDuplicateChar {
 
@@ -9,7 +8,7 @@ public class FindDuplicateChar {
         str=str.replace(" ","").toLowerCase();
 
         // print unique values
-        Set<Character> set=new HashSet<>();
+        Set<Character> set=new LinkedHashSet<>();
         for(char ch:str.toCharArray()){
                 set.add(ch);
         }
@@ -21,11 +20,32 @@ public class FindDuplicateChar {
     }
 
     public static void printDuplicate(String str){
+        Set<Character> dup=new LinkedHashSet<>();
+        Set<Character> set=new LinkedHashSet<>();
+        for(char ch:str.toCharArray()){
+              if(!set.contains(ch)){
+                  set.add(ch);
+              }else{
+                  dup.add(ch);
+              }
+        }
+        System.out.println(dup);
+    }
 
+    public static void printFrequencyofChar(String str){
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
     }
     public static void main(String[] args) {
         String str="Java Programming";
         printUniqueChars(str);
         printDuplicate(str);
+        printFrequencyofChar(str);
     }
 }
